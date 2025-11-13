34322c83215e88a510af9c2daac4b7a6391be79f
liam wohlstedt <liam@wohlstedt.com>
2025-11-13 14:32:09 +0100

    second

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(){
     const int size = 100;
     std::string foods[100];
 
-     fill(foods, foods + 100, "test");
-
+     fill(foods, foods + (size/2), "test");
+    fill(foods + (size/2), foods + size, "test2");
     for(std::string food : foods){   
         std::cout << food << '\n';
 
