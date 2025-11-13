819a5b1c76161b3aba2439e4f9caf4843aede157
liam wohlstedt <liam@wohlstedt.com>
2025-11-13 16:03:04 +0100

    fourth

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 
 int main(){
-    // fill(); 
-    const int size = 100;
-    std::string foods[100];
 
-    fill(foods, foods + (size/3), "test");
-    fill(foods + (size/3), foods + (size/3)*2, "test2");
-    fill(foods + (size/3)*2, foods + size, "test3");
-
-    for(std::string food : foods){   
-        std::cout << food << '\n';
-
-
+    std::string foods[5];
+    int size = sizeof(foods)/sizeof(foods[0]);
+    std::string temp;
+    for(int i = 0; i < size; i++ ){
+        std::cout << "enter a food you like or 'q' to quit #" << i << ": "; 
+        std::getline(std::cin, temp);
+        if(temp == "q"){
+            break;
+        }
+        else
+            foods[i] = temp;
+        
+    }
+    std::cout << "you like the following foods"; 
+    
+    for(int i = i; !foods[i].empty(); i++){
+        std::cout << foods[i] << '\n';
     }
 
 
-    return 0;
-}
 
 
+
+
+
+
+
+
+
+
+return 0; } 
